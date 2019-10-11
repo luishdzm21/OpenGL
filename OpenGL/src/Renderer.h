9471ab525a937b7ae4b712a2126d1b5fa8d06a38
Luis Alberto hernandez <luishdz_21@hotmail.com>
2019-10-11 13:32:59 -0500

    Created basic renderer

--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -2,6 +2,11 @@
 
 #include <GL/glew.h>
 
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 #define ASSERT(x) if (!(x)) __debugbreak(); 
 #define GLCall(x) GLClearError();\
 	x;\
@@ -9,3 +14,10 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
