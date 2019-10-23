c827e20206da151fb234d398bdd24797e4da8662
Luis Alberto hernandez <luishdz_21@hotmail.com>
2019-10-22 19:25:26 -0500

    Extending test framework

--- OpenGL/src/tests/Test.h
+++ OpenGL/src/tests/Test.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <vector>
+#include <functional>
+#include <string>
+
 namespace test {
 
-	class Test 
+	class Test
 	{
 	public:
 		Test() {}
@@ -13,4 +17,21 @@ namespace test {
 		virtual void OnImGuiRender() {}
 	};
 
+	class TestMenu : public Test
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+
+		void OnImGuiRender() override;
+
+		template<typename T> 
+		void RegisterTest(const std::string& name) {
+			std::cout << "Registering test " << name << std::endl;
+
+			m_Tests.push_back(std::make_pair(name, []() { return new T(); }));
+		}
+	private:
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::function<Test*()>>> m_Tests;
+	};
 }
